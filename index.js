7a5b57baba1e83f7f65d0b5a7a698146bbe113cd
Eva <yi.zheng1709@gmail.com>
2021-04-17 14:38:48 -0400

    commit tree a7d93fc9b353662d4b2b22b89f19eb8c8e455cbc

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+// // Add your code here
+// let formData = {
+//     name: "Eva",
+//     email: "eva@eva.com"
+// }
+
+// let config = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type" : "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+
+function submitData(name, email) {
+    return(
+        fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({name: name, email: email})
+        })
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            document.body.innerHTML = object.id
+        })
+        .catch(function(error) {
+            alert("There was an error!")
+            document.body.innerHTML += error.message
+        } 
+        )
+    )
+}
